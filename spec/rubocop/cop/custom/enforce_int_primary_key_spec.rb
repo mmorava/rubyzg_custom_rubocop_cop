6e61a87ade59181678463f3228edd579a39f7c53
Martin Morava <martin.morava@gmail.com>
2019-05-16 13:24:37 +0200

    Write cop specs

--- spec/rubocop/cop/custom/enforce_int_primary_key_spec.rb
+++ spec/rubocop/cop/custom/enforce_int_primary_key_spec.rb
@@ -2,4 +2,50 @@
 require_relative '../../../../rubocop/cop/custom/enforce_int_primary_key'
 
 describe RuboCop::Cop::Custom::EnforceIntPrimaryKey do
+  subject(:cop) { described_class.new(config) }
+  let(:config) { RuboCop::Config.new }
+
+  it 'does not register an offense if id is set to an integer' do
+    expect_no_offenses <<~RUBY
+      create_table :some_table_name, id: :integer
+    RUBY
+  end
+
+  context 'when create table is called with options argument' do
+    it 'registers an offense if id field type is not set to the integer' do
+      expect_offense <<~RUBY
+        create_table :some_table_name, primary_key: :some_id
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use an integer field type for id column when creating a new table.
+      RUBY
+    end
+
+    it 'registers an offense if table name is a string' do
+      expect_offense <<~RUBY
+        create_table "some_table_name", primary_key: :some_id
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use an integer field type for id column when creating a new table.
+      RUBY
+    end
+  end
+
+  context 'when create table is called without and options argument' do
+    it 'registers an offense if id field type is not set to the integer' do
+      expect_offense <<-RUBY
+        create_table :some_table_name do |t|
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use an integer field type for id column when creating a new table.
+          t.string :name
+          t.string :email
+        end
+      RUBY
+    end
+
+    it 'registers an offense if table name is a string' do
+      expect_offense <<-RUBY
+        create_table "some_table_name" do |t|
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use an integer field type for id column when creating a new table.
+          t.string :name
+          t.string :email
+        end
+      RUBY
+    end
+  end
 end
